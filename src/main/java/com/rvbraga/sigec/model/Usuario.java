6665b1a307dabfc5920d7409da309acd6e88ee39
Rafael <rafael.vieira.braga@gmail.com>
2023-01-14 18:19:12 -0300

    Inicio criação página inicial

--- src/main/java/com/rvbraga/sigec/model/Usuario.java
+++ src/main/java/com/rvbraga/sigec/model/Usuario.java
@@ -1,5 +1,25 @@
 package com.rvbraga.sigec.model;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario implements Serializable{/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	private String username;
+	private String password;
 
 }
