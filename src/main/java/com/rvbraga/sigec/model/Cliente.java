307c5c2c2908f2f104711fbf11e71cb349640635
Rafael <rafael.vieira.braga@gmail.com>
2023-01-13 22:07:31 -0300

    Criação de modelos Cliente e DOcumento

--- src/main/java/com/rvbraga/sigec/model/Cliente.java
+++ src/main/java/com/rvbraga/sigec/model/Cliente.java
@@ -4,16 +4,31 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
+@Entity
 public class Cliente implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	private String nome;
 	private Endereco endereco;
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Documento> documentos;
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Pagamento> pagamentos;
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Processo> processos;
 	
 
--- src/main/java/com/rvbraga/sigec/model/Documento.java
+++ src/main/java/com/rvbraga/sigec/model/Documento.java
@@ -1,5 +1,33 @@
 package com.rvbraga.sigec.model;
 
-public class Documento {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Documento implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	private String tipo;
+	private LocalDate dataRegistro;
+	private String orgaoExpeditor;
+	private String numero;
+	private LocalDate dataValidade;
+	@ManyToOne
+	private Cliente cliente;
 
 }
