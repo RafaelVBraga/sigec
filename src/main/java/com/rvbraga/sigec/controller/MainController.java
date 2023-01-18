7ad4cf730ca1b78d137aba548af5be811408a4a1
Rafael <rafael.vieira.braga@gmail.com>
2023-01-17 23:18:42 -0300

    Correção no controller e página Cliente.html

--- src/main/java/com/rvbraga/sigec/controller/MainController.java
+++ src/main/java/com/rvbraga/sigec/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,8 +56,8 @@ public class MainController {
 		return "cliente.html";
 	}
 	 
-	@GetMapping("/clientes/pesquisa")
-	public String pesquisaCliente(Model model,@RequestParam PesquisaDto pesquisa, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue ="3")int size) {
+	@PostMapping("/clientes/pesquisa")
+	public String pesquisaCliente(Model model,@ModelAttribute("pesquisa") PesquisaDto pesquisa, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue ="3")int size) {
 		System.out.println("Entrou no controller");
 		System.out.println(pesquisa.getPesquisaCpf());
 		System.out.println(pesquisa.getPesquisaNome());
