d36fe4b40f2b4b8e00b4eea50435ace260dece1d
Rafael <rafael.vieira.braga@gmail.com>
2024-03-19 04:28:57 -0300

    Atualização da pagina clientes e ajustes nos layouts das páginas documentos e pagamentos

--- src/main/resources/static/js/cliente/clientes.js
+++ src/main/resources/static/js/cliente/clientes.js
@@ -2,3 +2,11 @@ function atribuir(e){
 	var input_id = document.getElementById('testeId');
 	input_id.setAttribute("text",e);	
 }
+
+const myModal = document.getElementById('modalProc')
+const myInput = document.getElementById('showModalProc')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
