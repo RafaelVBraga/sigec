c80a62a21d4b9bd53c04c20641c8620b8b4c894a
Rafael <rafael.vieira.braga@gmail.com>
2024-04-10 18:15:03 -0300

    Update de fonte e fix na página de parcelas

--- src/main/java/com/rvbraga/sigec/model/Pagamento.java
+++ src/main/java/com/rvbraga/sigec/model/Pagamento.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -41,6 +42,7 @@ public class Pagamento implements Serializable{/**
 	@ManyToOne
     @JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	@Valid
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Parcela> parcelas;
 
